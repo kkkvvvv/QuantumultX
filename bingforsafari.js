0e0efe25616f6df9b4f70346e1a4ae5d4145da18
kkkvvvv <john19930207@gmail.com>
2023-05-20 15:15:56 +0800

    Update bingforsafari.js

--- bingforsafari.js
+++ bingforsafari.js
@@ -1,6 +1,6 @@
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['User-Agent'] = '1';
+modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/113.0.1774.42 Version/14.0 Mobile/15E148 Safari/604.1';
 
 
 
