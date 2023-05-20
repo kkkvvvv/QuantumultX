abbed1dbd18e3d789ee954cb0becbcec6f8fa60b
kkkvvvv <john19930207@gmail.com>
2023-05-20 14:30:51 +0800

    bingforsafari.js

--- bingforsafari.js
+++ bingforsafari.js
@@ -1,3 +1,4 @@
+#https://raw.githubusercontent.com/kkkvvvv/Filter/main/bingforsafari.js
 var modifiedHeaders = $request.headers;
 
 modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/110.0.1587.57 Version/15.0 Mobile/15E148 Safari/604.1';
