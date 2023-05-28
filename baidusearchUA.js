973520c131f8ab5fb8756428956f889b788febd4
kkkvvvv <john19930207@gmail.com>
2023-05-28 17:28:02 +0800

    Create baidusearchUA.js

---
+++ baidusearchUA.js
@@ -0,0 +1,7 @@
+var modifiedHeaders = $request.headers;
+
+modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/110.0.1587.57 Version/15.0 Mobile/15E148 Safari/604.1 SearchCraft/2.8.2';
+
+
+
+$done({headers : modifiedHeaders});
