bfa5af9d072d6fabcc41f344f36d3f51e056abf0
kkkvvvv <john19930207@gmail.com>
2023-05-31 23:39:51 +0800

    Update bingforsafari2.js

--- Rewrite/bingforsafari2.js
+++ Rewrite/bingforsafari2.js
@@ -1,6 +1,6 @@
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/110.0.1587.57 Version/15.0 Mobile/15E148 Safari/604.1';
+modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.8 Mobile/15E148 Safari/605.1.15 BingSapphire/1.0.410505003';
 
 
 
