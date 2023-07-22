be0fd2871d2f630e336f2dbd3e5c3db51ab0ad14
kkkvvvv <john19930207@gmail.com>
2023-07-22 21:29:23 +0800

    Delete bingforsafari2.js

--- Rewrite/bingforsafari2.js
+++
@@ -1,7 +0,0 @@
-var modifiedHeaders = $request.headers;
-
-modifiedHeaders['User-Agent'] = Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1';
-
-
-
-$done({headers : modifiedHeaders});
