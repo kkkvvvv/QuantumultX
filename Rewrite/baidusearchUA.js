3f6e65792c81cb9ad0ce0e5a9a374f0b854b9ba8
kkkvvvv <john19930207@gmail.com>
2025-02-13 14:46:10 +0800

    更新 baidusearchUA.js

--- Rewrite/baidusearchUA.js
+++ Rewrite/baidusearchUA.js
@@ -1,6 +1,6 @@
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/110.0.1587.57 Version/15.0 Mobile/15E148 Safari/604.1 SearchCraft/2.8.2';
+modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/110.0.1587.57 Version/15.0 Mobile/15E148 Safari/604.1 SearchCraft/5.12.0.10';
 
 
 
